af06e8b81c33e149d97c3f20f898d027cb4757f6
Ricardo_Velez <2000rvelez@gmail.com>
2024-02-03 15:35:41 -0500

    disable exhaustive deps rule in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
   "rules": {
     "@typescript-eslint/consistent-type-imports": "off",
-    "@typescript-eslint/no-unused-vars": "off"
+    "@typescript-eslint/no-unused-vars": "off",
+    "react-hooks/exhaustive-deps": "off",
   }
 };
