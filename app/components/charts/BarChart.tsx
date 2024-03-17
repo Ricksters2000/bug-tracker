1b769609005a264dacc5c71c02228d162cf8170c
Ricardo_Velez <2000rvelez@gmail.com>
2024-03-17 14:22:20 -0400

    fix type errors in bar chart

--- app/components/charts/BarChart.tsx
+++ app/components/charts/BarChart.tsx
@@ -4,19 +4,18 @@ import {ChartData, ChartOptions} from 'chart.js';
 import {ChartDatasetsRaw} from './utils/convertDataToChartData';
 import {objectKeysAndExcludeKey} from '~/utils/objectKeys';
 
-type Props<T extends Record<string, number | string>, K extends keyof T> = {
+type Props<T extends Record<string, number>, K extends keyof T> = {
   datasetsRaw: ChartDatasetsRaw<T, K>;
 }
 
 export const BarChart = <T extends Record<string, number>, K extends keyof T>(props: Props<T, K>) => {
   const [datasets, setDatasets] = React.useState<ChartData<`bar`, number[]>[`datasets`]>([])
-  const [labels, setLabels] = React.useState<Array<string>>(props.datasetsRaw.labels)
 
   React.useEffect(() => {
     const datasetsRaw = props.datasetsRaw
     const keys = objectKeysAndExcludeKey(datasetsRaw, `labels`)
     const currentDatasets: ChartData<`bar`, number[]>[`datasets`] = keys.map(key => {
-      const data = datasetsRaw[key]
+      const data = datasetsRaw[key] as Array<number>
       return {
         label: key.toString(),
         data: data,
@@ -26,7 +25,7 @@ export const BarChart = <T extends Record<string, number>, K extends keyof T>(pr
   }, [])
   const data: ChartData<`bar`, number[], string> = {
     datasets: datasets,
-    labels: labels,
+    labels: props.datasetsRaw.labels,
   }
   const options: ChartOptions<`bar`> = {
     indexAxis: `y`,
