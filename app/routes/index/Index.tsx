c37529114024529c5e69d9d14fe6e9cb3e6b59e1
Ricardo_Velez <2000rvelez@gmail.com>
2024-04-09 20:15:51 -0400

    replace some of the dummy text for real descriptions

--- app/routes/index/Index.tsx
+++ app/routes/index/Index.tsx
@@ -1,4 +1,4 @@
-import 'aos/dist/aos.css';
+import aosCss from 'aos/dist/aos.css';
 import React from 'react';
 import emotionStyled from '@emotion/styled';
 import AOS from 'aos';
@@ -6,6 +6,7 @@ import {Box, Button, Grid, Typography} from '@mui/material';
 import {MainFeatureCard} from './components/MainFeatureCard';
 import {UserIcon} from '~/assets/icons/UserIcon';
 import {SubFeatureCard} from './components/SubFeatureCard';
+import {LinksFunction} from '@remix-run/node';
 
 export default function Index() {
   React.useEffect(() => {
@@ -23,8 +24,12 @@ export default function Index() {
               <Grid container marginTop={`-32px`} marginLeft={`-32px`} width={`calc(100% + 32px)`} spacing={4}>
                 <Grid item xs={12} md={6}>
                   <Box data-aos="fade-right">
-                    <Typography marginBottom={`16px`} variant='h2' fontWeight={700}>Easily manage your projects with Project Flow</Typography>
-                    <Typography marginBottom={`24px`} fontSize={`1.25rem`} lineHeight={1.6}>Description for thing</Typography>
+                    <Typography marginBottom={`16px`} variant='h2' fontWeight={700}>
+                      The bug tracker tool to manage all your projects
+                    </Typography>
+                    <Typography marginBottom={`24px`} fontSize={`1.25rem`} lineHeight={1.6}>
+                      Easily track all your tasks andbugs between an unlimited amount of projects.
+                    </Typography>
                     <Box>
                       <Button variant='outlined'>Start Now</Button>
                     </Box>
@@ -45,8 +50,8 @@ export default function Index() {
                 <Grid item xs={12} md={4}>
                   <MainFeatureCard
                     icon={<UserIcon/>}
-                    title='Lorum Ipsum'
-                    description='Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum '
+                    title='Responsive user-friendly UI'
+                    description='Simple and easy to understand user-interface which is also responsive to be used on any screen size.'
                   />
                 </Grid>
                 <Grid item xs={12} md={4}>
@@ -76,7 +81,7 @@ export default function Index() {
                   The powerful and flexible tool for managing projects
                 </Typography>
                 <Typography fontSize={`1.25rem`} textAlign={`center`} color={`rgb(86, 89, 115)`}>
-                  Manage a limitless amount of projects and other stuff
+                  Manage a limitless amount of projects and many more!
                 </Typography>
               </Box>
               <Box marginTop={`48px`}>
@@ -84,28 +89,28 @@ export default function Index() {
                   <Grid item xs={12} sm={6} md={4}>
                     <SubFeatureCard
                       icon={<UserIcon/>}
-                      title='Lorum Ipsum'
-                      description='Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum'
+                      title='Analytics'
+                      description='View analytics for all your projects at the same time and get an understanding as to which ones need more focus on.'
                     />
                   </Grid>
                   <Grid item xs={12} sm={6} md={4}>
                     <SubFeatureCard
                       icon={<UserIcon/>}
-                      title='Lorum Ipsum'
-                      description='Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum'
+                      title='Assigning Users'
+                      description='Assign users to tickets and projects to simply allow them to focus on specific task needed to be done.'
                     />
                   </Grid>
                   <Grid item xs={12} sm={6} md={4}>
                     <SubFeatureCard
                       icon={<UserIcon/>}
-                      title='Lorum Ipsum'
-                      description='Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum'
+                      title='Manage User Roles'
+                      description='Give users their own roles to limit the features they have access to in the team.'
                     />
                   </Grid>
                   <Grid item xs={12} sm={6} md={4}>
                     <SubFeatureCard
                       icon={<UserIcon/>}
-                      title='Lorum Ipsum'
+                      title='Advanced Ticket Filter'
                       description='Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum'
                     />
                   </Grid>
